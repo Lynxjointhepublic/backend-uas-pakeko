bb4fb6fc7ae8d2aaa10219dd01b2c57c9cd4b436
Lynxjointhepublic <albidaniel34@gmail.com>
2022-07-06 16:53:02 +0700

    upbackend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const { MongoClient } = require('mongodb');
 const app = express ();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const url = 'mongodb+srv://lynx:epok2562002@cluster0.osrmk.mongodb.net/test';
 const client = new MongoClient(url);
